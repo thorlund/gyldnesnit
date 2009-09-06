49cc2a2ebced4a78d47ca5b56169f7455d5819ec
bonde <ulrikbonde@gmail.com>
2009-09-06 21:20:33 +0200

    Minor update

--- src/concepts/goldenLibrary.py
+++ src/concepts/goldenLibrary.py
@@ -22,7 +22,6 @@ def __getintersection__(x1, x2, x3, x4, y1, y2, y3, y4):
 
 	return cv.cvPoint(px, py)
 	
-
 class line():
 	"""A container for a line segment."""
 	def __init__(self, p1, p2):
@@ -38,18 +37,23 @@ class line():
 def intersection(line1, line2):
 	"""Get the intersection of two lines as a cvPoint"""
 	x1 = line1.p1.x
-	x2 = line1.p2.x
-	x3 = line2.p1.x
-	x4 = line2.p2.x
 	y1 = line1.p1.y
+
+	x2 = line1.p2.x
 	y2 = line1.p2.y
+
+	x3 = line2.p1.x
 	y3 = line2.p1.y
+
+	x4 = line2.p2.x
 	y4 = line2.p2.y
 
 	return __getintersection__(x1, x2, x3, x4, y1, y2, y3, y4)
 
 def findMeans(size):
-	"""Returns four line segments ((x, y), (u,v)) marking the golden mean"""
+	"""Return (four) line segments marking the golden mean"""
+	
+	# Array holding the line segments
 	lines = []
 
 	# First vertical lines (along x-axis)
@@ -77,6 +81,7 @@ def findMeans(size):
 	p1 = cv.cvPoint(0, (size.height - dy))
 	p2 = cv.cvPoint(size.width, (size.height - dy))
 	lines.append(line(p1, p2))
+
 	return lines
 
 def drawLines(lines, outimage):
