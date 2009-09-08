bbaebab71bfc913bb6436ce0ce27d10a1a7e0a51
Ulrik Bonde <ulrikbonde@gmail.com>
2009-09-08 21:42:19 +0200

    Added bindings for edge detecting. Amazing results!

--- src/tools/goldenLibrary.py
+++ src/tools/goldenLibrary.py
@@ -89,3 +89,43 @@ def drawLines(lines, outimage):
 	col = cv.CV_RGB(255, 0, 0)
 	for line in lines:
 		cv.cvLine(outimage, line.p1, line.p2, col, 1, 8, 0)
+
+def findEdges(original, threshold = 100):
+	"""Return a new edge detected image with a specified threshold"""
+	# First create the out picture, the one the method will return
+	out = cv.cvCreateImage(cv.cvGetSize(original), 8, 3)
+
+	# Create two pictures with only one channel for a b/w copy
+	# and one for storring the edges found in the b/w picture
+	gray = cv.cvCreateImage(cv.cvGetSize(original), 8, 1)
+	edge = cv.cvCreateImage(cv.cvGetSize(original), 8, 1)
+
+	# Create the b/w copy of the original
+	cv.cvCvtColor(original, gray, cv.CV_BGR2GRAY)
+
+	# Blur the b/w copy, but put the result into edge pic
+	cv.cvSmooth(gray, edge, cv.CV_BLUR, 3, 3, 0)
+
+	# Negate the b/w copy of original with newly blurred
+	# b/w copy. This will make egdes stand out
+	# NOTE:	Both images are "not'ed" against each other
+	#	meaning that the b/w copy will now be blurred!
+	cv.cvNot(gray, edge)
+
+	# Run an edge-finding algorithm called 'Canny'
+	# It will analyse the first argument and store the
+	# resulting picture in the second argument
+	cv.cvCanny(gray, edge, threshold, threshold * 3, 3)
+
+	# We initialize our out-image to black
+	cv.cvSetZero(out)
+
+	# Finally, we use the found edges, which are b/w, as
+	# a mask for copying the colored edges from the original
+	# to the out-image
+	cv.cvCopy(original, out, edge)
+
+	# Et voilà! The skin comes right off!
+	# http://www.youtube.com/watch?v=rUbWjIKxrrs
+	return out
+
