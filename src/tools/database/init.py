4fcd0ad67cdd31f6db603176314376fe1c301361
thorlund <thorlund@diku.dk>
2009-09-16 20:46:59 +0200

    startet arbejdet paa init scriptet til databasen

---
+++ src/tools/database/init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import os # For opening files
+import pysqlite # Google python db api 2.0, this is the sqllite 
+
+##
+# This should init the database with the connection string given below
+#
+#
+#
+##
+
+user =
