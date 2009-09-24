5716e65f1c848ff03c5c64cddc0d3369e3e6b386
Ulrik Bonde <ulrikbonde@gmail.com>
2009-09-24 23:21:20 +0200

    Refined tests

--- src/tests/basicFunctionalityTest.py
+++ src/tests/basicFunctionalityTest.py
@@ -21,7 +21,7 @@ print ""
 
 if len(sys.argv) < 2:
 	print "Input error!"
-	print "Usage: ./lineScannerTest.py <filename> [threshold1] [threshold2]"
+	print "Usage: ./basicFunctionalityTest.py <filename>"
 	print "The above means that you have to supply a path to an image"
 	print ""
 	exit(-1)
@@ -32,40 +32,29 @@ print ""
 
 filename = sys.argv[1]
 
-threshold2 = None
-
-if len(sys.argv) == 3:
-	threshold1 = int(sys.argv[2])
-elif len(sys.argv) == 4:
-	threshold1 = int(sys.argv[2])
-	threshold2 = int(sys.argv[3])
-else:
-	threshold1 = 100
-
 image = highgui.cvLoadImage (filename)
-out = edgeDetector.findEdges(image, threshold1, threshold2)
 
 if not image:
 	print "Error loading image '%s'" % filename
 	print ""
 	sys.exit(-1)
 
-winname = "lineScannerTest"
+winname = "basicFunctionalityTest"
 
 highgui.cvNamedWindow (winname, highgui.CV_WINDOW_AUTOSIZE)
 
 print "Finding the golden means in the picture"
 lines = lib.findMeans(cv.cvGetSize(image))
 
+print "Drawing the means"
+lib.drawLines(lines, image)
 
-print "Test plot and line scanner methods"
-points = lineScanner.naiveLineScanner(out, image, lines[0])
-
-for point in points:
-	lib.plot(out, point, 2)
+print "Test plot function and intersection function"
+lib.plot(image, lines[0].intersection(lines[2]))
+lib.plot(image, lib.intersection(lines[1], lines[3]), color=lib.COL_BLUE)
 
 while True:
-	highgui.cvShowImage (winname, out)
+	highgui.cvShowImage (winname, image)
 
 	c = highgui.cvWaitKey(0)
 	
--- src/tests/edgeDetectorTest.py
+++ src/tests/edgeDetectorTest.py
@@ -2,6 +2,12 @@
 #
 # Ulrik Bonde
 
+print "" # Just for good meassure
+# INIT THE TEST
+# THIS SHOULD BE FIRST AS IT WILL MAKE THE LIBRARIES BELLOW AVAILABLE
+import initTests
+initTests.init()
+
 import sys
 import lib.goldenLibrary as lib
 import lib.edgeDetector as edgeDetector
@@ -11,14 +17,12 @@ import lib.lineScanner as lineScanner
 from opencv import cv
 from opencv import highgui
 
-# INIT THE TEST
-initTests.init()
 
 print ""
 
 if len(sys.argv) < 2:
 	print "Input error!"
-	print "Usage: ./testLibrary.py <filename> [threshold1] [threshold2]"
+	print "Usage: ./edgeDetectorTest.py <filename> [threshold1] [threshold2]"
 	print "The above means that you have to supply a path to an image"
 	print ""
 	exit(-1)
@@ -40,31 +44,18 @@ else:
 	threshold1 = 100
 
 image = highgui.cvLoadImage (filename)
-out = edgeDetector.findEdges(image, threshold1, threshold2)
 
 if not image:
 	print "Error loading image '%s'" % filename
 	print ""
 	sys.exit(-1)
 
-winname = "Test golden mean"
+print "Finding edges using Canny"
+out = edgeDetector.findEdges(image, threshold1, threshold2)
+
+winname = "edgeDetectorTest"
 
 highgui.cvNamedWindow (winname, highgui.CV_WINDOW_AUTOSIZE)
-
-print "Finding the golden means in the picture"
-lines = lib.findMeans(cv.cvGetSize(image))
-
-
-print "Drawing the means"
-#lib.drawLines(lines, out)
-
-print "Test plot function and intersection function"
-#lib.plot(out, lines[0].intersection(lines[2]))
-points = lineScanner.naiveLineScanner(out, image, lines[0])
-
-for point in points:
-	lib.plot(out, point, 2)
-#lib.plot(out, lib.intersection(lines[1], lines[3]), color=lib.COL_BLUE)
 
 while True:
 	highgui.cvShowImage (winname, out)
