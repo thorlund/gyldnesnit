cc5d18c838efe25c51852706ce1a0fdd3095ec8b
steenstrup <kasper.steenstrup@gmail.com>
2009-09-30 21:24:10 +0200

    floofill færdig

--- src/lib/featureDetector.py
+++ src/lib/featureDetector.py
@@ -5,6 +5,75 @@ Provides a method for finding features
 # Import what we need from OpenCV
 from opencv import cv
 
+# import random
+import random
+
+# import goldenLibrary
+import lib.goldenLibrary as lib
+
+
+
+def floodFillLine(out, points, line, lo, up):
+	"""take are golden ratio and make floot fill betvine alle of the points
+points are the points on the given line"""
+	#Set the starting point at the start of the line
+	startpoint = line.getPoints()[0]
+	
+	#Set the ending point at the ending af the line
+	points.append(line.getPoints()[1])
+	
+	for point in points:
+		out = floodFillBetweenPoints(out, lo, up, startpoint, point, line)
+		startpoint = point
+		
+	for point in points:
+		lib.plot(out, point, 2)
+	
+	return out
+
+# Floofill the image at point x,y whit are lower and upper thres hold namt lo and up
+# Start and stop point is the point that the def runs from to
+# golden_ratio is are indekater of whot of the 4 golden ratio we are working in
+# 0 and 1 is from the top to the bottom.
+# 2 and 3 is from the left to til rigth
+def floodFillBetweenPoints(image, lo, up, start_point, stop_point, line):
+	"""Floofill the image at point x,y whit are lower and upper thres hold namt lo and up
+Start and stop point is the point that the def runs from to"""		
+	# the color of the blob
+	b = random.randint(0,255)
+	g = random.randint(0,255)
+	r = random.randint(0,255)
+	color = cv.CV_RGB(r,g,b);
+	
+	#Sets the flag
+	flags = 4 + (255 << 8) + cv.CV_FLOODFILL_FIXED_RANGE
+
+	#Sets are observer on the blob
+	comp = cv.CvConnectedComp()
+	
+	(p1, p2) = line.getPoints()
+	if p1.x == p2.x:
+		#Set the seed to the start of the golden ratio 
+		seed = cv.cvPoint(start_point.x,start_point.y+1)
+		
+		#Color betwine start_point+1 and point-1  
+		for i in range(start_point.y+1, stop_point.y-1):
+			cv.cvFloodFill(image, seed, color, cv.CV_RGB(lo,lo,lo), cv.CV_RGB(up,up,up),comp)# ,flags, None);
+			seed = cv.cvPoint(seed.x,seed.y+1)
+	else:
+		#Set the seed to the start of the golden ratio 
+		seed = cv.cvPoint(start_point.x+1,start_point.y)
+		
+		#Color betwine start_point+1 and point-1  
+		for i in range(start_point.x+1, stop_point.x-1):
+			cv.cvFloodFill(image, seed, color, cv.CV_RGB(lo,lo,lo), cv.CV_RGB(up,up,up),comp)# ,flags, None);
+			seed = cv.cvPoint(seed.x+1,seed.y)
+	
+	#Diffrent forms of comp print
+	#print "%g pixels were repainted" % comp.area;
+	#print "%rect" % comp.rect;
+	return image 
+	
 def getGoodFeatures(image):
 	# XXX: BETA but working
 	# TODO: Clean up!! Comments properly
---
+++ src/tests/floofilltest.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+#
+# Kasper steenstrup
+
+print "" # Just for good meassure
+# INIT THE TEST
+# THIS SHOULD BE FIRST AS IT WILL MAKE THE LIBRARIES BELLOW AVAILABLE
+import initTests
+initTests.init()
+
+import sys
+import lib.goldenLibrary as lib
+import lib.floofill as floofill
+import lib.edgeDetector as edgeDetector
+import lib.lineScanner as lineScanner
+import lib.featureDetector as featureDetector
+
+# import the necessary things for OpenCV
+from opencv import cv
+from opencv import highgui
+#from opencv.cv import *
+
+print "Testing library"
+print "Press 'q' to exit"
+print ""
+
+filename = sys.argv[1]
+if len(sys.argv) == 4:
+	lo = int(sys.argv[2])
+	up = int(sys.argv[3])
+	x = 200
+	y = 200
+elif len(sys.argv) == 6:
+	lo = int(sys.argv[2])
+	up = int(sys.argv[3])
+	x = int(sys.argv[4])
+	y = int(sys.argv[5])
+else:
+	lo = 7
+	up = 7
+	x = 200
+	y = 200
+
+image = highgui.cvLoadImage (filename)
+
+if not image:
+	print "Error loading image '%s'" % filename
+	print ""
+	sys.exit(-1)
+
+threshold1 = 70;
+threshold2 = 70;
+out = edgeDetector.findEdges(image, threshold1, threshold2)
+
+print "Finding the golden means in the picture"
+
+lines = lib.findMeans(cv.cvGetSize(image))
+
+print "Test plot and line scanner methods"
+points = lineScanner.naiveLineScanner(out, image, lines[0])
+
+out = highgui.cvLoadImage (filename)
+
+out = featureDetector.floodFillLine(out, points, lines[0], lo, up)
+
+#startpoint = lines[0].getPoints()[0]
+#points.append(lines[0].getPoints()[1])
+#for point in points:
+#	out = floofill.floofill(out, lowerThres, upperThres, startpoint, point, 1)
+#	startpoint = point
+#for point in points:
+#	lib.plot(out, point, 2)
+#out = edgeDetector.findEdges(out, 70, 70)
+
+winname = "floot"
+
+highgui.cvNamedWindow (winname, highgui.CV_WINDOW_AUTOSIZE)
+
+while True:
+	highgui.cvShowImage (winname, out)
+
+	c = highgui.cvWaitKey(0)
+	
+	if c == 'q':
+		print "Exiting ..."
+		print ""
+		sys.exit(0)
