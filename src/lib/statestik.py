beaf4cab40b912b5e181bc575791923c85388312
steenstrup <kasper.steenstrup@gmail.com>
2009-10-07 20:07:23 +0200

    FF på begge sider

---
+++ src/lib/statestik.py
@@ -0,0 +1,22 @@
+# Import what we need from OpenCV
+from opencv import cv
+
+# import goldenLibrary
+import lib.goldenLibrary as lib
+	
+def splitOpBlob(floodFillImage,copy, points, line):
+	comp = cv.CvConnectedComp()
+	# Get start and stop points
+	(start_point, stop_point) = line.getPoints()
+	for point in points:
+		# Get new random color
+		color = lib.getRandomColor()
+		if (start_point.x == stop_point.x):
+			print 'ny pounkt'
+			slice1 = floodFillImage[:,0:start_point.x+1]
+			cv.cvFloodFill(slice1, cv.cvPoint(start_point.x, start_point.y+1), color, cv.CV_RGB(2,2,2), cv.CV_RGB(2,2,2),comp)
+			print comp.area
+			slice2 = copy[:,start_point.x:]
+			cv.cvFloodFill(slice2, cv.cvPoint(0, start_point.y+1), color, cv.CV_RGB(2,2,2), cv.CV_RGB(2,2,2),comp)
+			print comp.area
+			start_point = point
