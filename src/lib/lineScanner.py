bf686038b0859b40ca984bd9337e2d963cfec171
bonde <ulrikbonde@gmail.com>
2009-10-16 21:32:33 +0200

    Added a line scanner for bw images
    As the feature detector now use a bw edge image the original line
    scanner no longer worked. Is was operating on rgb where bw only has one
    channel. New method is called naiveLineScannerBW or something like what.

--- src/lib/lineScanner.py
+++ src/lib/lineScanner.py
@@ -10,9 +10,8 @@ from opencv import cv
 
 def naiveLineScanner(edgeImage, line):
 	"""Scan for edges along a line in a edge-detected image
-
-This is _very_ naive
-Traverse a line, put the coordinate in a list if the pixel is white
+	This is _very_ naive
+	Traverse a line, put the coordinate in a list if the pixel is white
 """
 	# Get the points from the line
 	(p1, p2) = line.getPoints()
@@ -39,6 +38,39 @@ Traverse a line, put the coordinate in a list if the pixel is white
 	for point in slice:
 		thisColor = cv.CV_RGB(int(point[0]), int(point[1]), int(point[2]))
 		if not lib.isSameColor(lib.COL_BLACK, thisColor):
+			# Save this point
+			points.append(p)
+		# Update the coordinate
+		p = (cv.cvPoint(p.x + dx, p.y + dy))
+
+	return points
+
+def naiveBWLineScanner(edgeImageBW, line):
+	"""B/W version of the naiveLineScanner"""
+	# Get the points from the line
+	(p1, p2) = line.getPoints()
+
+	# We check the direction of the line then
+	# get the appropriate row or column.
+	# XXX: Any smarter way around this problem??
+	if p1.y == p2.y:
+		# Get the row, defined by y
+		dx = 1
+		dy = 0
+		slice = edgeImageBW[p1.y]
+	elif p1.x == p2.x:
+		# Get the column, defined by x
+		dx = 0
+		dy = 1
+		slice = edgeImageBW[:,p1.x]
+	else:
+		raise lib.OrientationException("The orientation is fucked up")
+
+	p = p1
+	points = []
+	# Now we can traverse every point in the row/column
+	for point in slice:
+		if not int(point) == 0:
 			# Save this point
 			points.append(p)
 		# Update the coordinate
