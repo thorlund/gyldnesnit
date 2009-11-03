9ca5d7178b30bb4338f6508df8e4947dc05527d1
steenstrup <kasper.steenstrup@gmail.com>
2009-11-03 15:36:13 +0100

    magincalculader nu mere færdig

--- src/lib/marginCalculader.py
+++ src/lib/marginCalculader.py
@@ -1,7 +1,7 @@
 import math
 # marginCalcualder taks are list of rations and return the max margin
 # the lines can have i %
-def marginCalculader(Rlist)
+def getPercentage(Rlist):
 	size = len(Rlist)
 	#the max magin that the function can return
 	tmp = 100
@@ -9,4 +9,16 @@ def marginCalculader(Rlist)
 		for j in range(0, size):
 			if abs(Rlist[i] - Rlist[j]) < tmp and not(j == i):
 				tmp = abs(Rlist[i] - Rlist[j])
-	return (tmp*100)/2.0
+	return (tmp)/2.0
+	
+def getPixels(image, line, marginPercentage):
+	
+	(p1, p2) = line.getPoints()
+	if p1.x == p2.x:
+		# Initialize the seed and set deltas for increasing the seed
+		tmp = int(image.width * marginPercentage)
+	elif p1.y == p2.y:
+		tmp = int(image.height * marginPercentage)
+	else:
+		raise lib.OrientationException("Unknown orientation")
+	return tmp
