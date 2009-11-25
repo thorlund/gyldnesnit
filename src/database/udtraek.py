293a701b0ddf8a146297581b220a7739bb8453ef
thorlund <thorlund@diku.dk>
2009-11-25 13:09:22 +0100

    Skal lige have det her fixet

--- src/database/udtraek.py
+++ src/database/udtraek.py
@@ -14,9 +14,15 @@ results = m.Result.select(m.Result.q.run==2)
 
 numberOfRegions = 0
 amountOfPictures = 0
+categories=dict()
 for result in results:
 	numberOfRegions = numberOfRegions + result.numberOfRegions
 	amountOfPictures = amountOfPictures +1
+	if result.numberOfRegions not in categories.keys():
+		categories[result.numberOfRegions]=1
+	else:
+		categories[result.numberOfRegions]= categories[result.numberOfRegions]+1
 
 print ('There was %i regions detected in %i pictures',(numberOfRegions,amountOfPictures))
 print 'average was :%i',(numberOfRegions/amountOfPictures)
+print categories
