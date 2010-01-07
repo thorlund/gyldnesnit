2d9ee0da7bf775eb3ae51c91b4ecda753c10afd9
bonde <ulrikbonde@gmail.com>
2010-01-07 16:15:37 +0100

    Omformuleret hypoteser og lavet noget graf-analyse på antal regioner

--- doc/rapport/afsnit/resultater/totalregions.r
+++ doc/rapport/afsnit/resultater/totalregions.r
@@ -3,6 +3,7 @@
 
 d0 <- read.table("csv/totalRegions.csv", sep=",", header=TRUE);
 prod = d0$interessante.regioner*d0$antal;
+prob = prod/sum(prod);
 #barplot(d0$antal,beside=TRUE, names=d0$interessante.regioner,cex.names=1)
 #print("Middelværdi");
 #print(mean(d0$antal));
@@ -22,24 +23,53 @@ for (i in d0$antal) {
     pos = pos + 1;
     start = stop;
 }
+# Remove the last element
 Regioner <- Regioner[1:length(Regioner) - 1];
-print(Regioner);
-print(length(Regioner));
+
+print("Antal maleier");
 print(sum(d0$antal));
+print("Total regioner");
+print(sum(prod));
 print("Middelværdi");
 print(mean(Regioner));
 print("Standardafvigelse");
 print(sd(Regioner));
+t.test(Regioner);
 
 # Plot
-png("billeder/totalregions.png");
-d0$antal[1] = 0;
+png("billeder/totalregions_var.png");
 plot(d0$interessante.regioner, d0$antal, "h", ylab="Antal malerier", xlab="Antal fundne insteressante regioner");
 dev.off();
 
-png("billeder/hist_totalregions.png");
-hist(Regioner, nclass=15, prob=TRUE);
-#yval = seq(1,600);
-#fval <- dnorm(yval, mean=mean(obs), sd=sd(obs));
+
+# Plot
+png("billeder/totalregions.png");
+yval = seq(1,600);
+d0$antal[1] = 0;
+#fval <- sum(d0$antal)*dnorm(yval, mean=mean(Regioner), sd=sd(Regioner));
+plot(d0$interessante.regioner, d0$antal, "h", ylab="Antal malerier", xlab="Antal fundne insteressante regioner");
 #points(yval, fval, type="l");
+dev.off();
+
+#Regioner <- Regioner[299:length(Regioner)];
+png("billeder/hist_totalregions.png");
+hist(Regioner, nclass=50, prob=TRUE);
+#barplot(prob, beside=TRUE,cex.names=1)
+#plot(d0$interessante.regioner, prob, "h", ylab="Antal malerier", xlab="Antal fundne insteressante regioner");
+#yval = seq(1,600);
+fval <- dexp(yval, rate = 1/mean(Regioner), log = FALSE)
+#fval <- dnorm(yval, mean=mean(Regioner), sd=sd(Regioner));
+points(yval, fval, type="l");
+dev.off();
+
+Regioner <- Regioner[299:length(Regioner)];
+
+png("billeder/qq_totalregions.png");
+qqnorm(Regioner);
+qqline(Regioner);
+dev.off();
+
+png("billeder/qq_log_totalregions.png");
+qqnorm(log(Regioner));
+qqline(log(Regioner));
 dev.off();
