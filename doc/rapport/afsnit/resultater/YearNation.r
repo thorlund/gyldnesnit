3ae9fa5120e7d0f794e3be07a61f0068eec648eb
Steenstrup <steenstrup@steenstrup-laptop.(none)>
2010-01-11 21:20:42 +0100

    2 dejlige billeder

---
+++ doc/rapport/afsnit/resultater/YearNation.r
@@ -0,0 +1,17 @@
+d0 <- read.table("csv/nation.csv", sep=",",header=TRUE)
+d1 <- read.table("csv/year.csv", sep=",",header=TRUE)
+
+png("billeder/nation.png", height = 480, width = 800)
+par(las=3, lheight =6, mai=c(2,1,1,1))
+barplot(d0$interessante.regioner,beside=TRUE,names=d0$ratios,  cex.axis=1.0, cex.names = 1.4, ylab = "Antal Billeder" )
+par(las=1)
+mtext(1, text = "Nationalitet", line = 8)
+dev.off()
+
+png("billeder/year.png", height = 480, width = 800)
+par(las=3, lheight =6, mai=c(2,1,1,1))
+barplot(d1$interessante.regioner,beside=TRUE,names=d1$ratios, cex.axis=1.0,  cex.names = 1.4, ylab = "Antal Billeder"  )
+par(las=1)
+mtext(1, text = "År", line = 8)
+dev.off()
+q()
