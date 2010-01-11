9e8bfdc6fdec7ac6eec060e802560a43fe02e976
Steenstrup <steenstrup@steenstrup-laptop.(none)>
2010-01-11 22:06:49 +0100

    sorteret graf

--- doc/rapport/afsnit/resultater/goldenyear.r
+++ doc/rapport/afsnit/resultater/goldenyear.r
@@ -5,7 +5,7 @@ d3 <- read.table("csv/yearcut3U.csv", sep=",",header=TRUE)
 
 png("billeder/yearcut0U.png", height = 480, width = 800)
 par(las=3, lheight =6, mai=c(2,1,1,1))
-barplot(d0$interessante.regioner,beside=TRUE,names=d1$ratios,  cex.axis=1.0, cex.names = 1.4, ylab = "Antal regioner" )
+barplot(d0$interessante.regioner,beside=TRUE,names=d0$ratios,  cex.axis=1.0, cex.names = 1.4, ylab = "Antal regioner" )
 par(las=1)
 mtext(1, text = "År", line = 8)
 dev.off()
