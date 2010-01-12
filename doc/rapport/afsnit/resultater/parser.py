5bf2b3e1256d191441a4a807e71bbe208fdadb8b
Steenstrup <steenstrup@steenstrup-laptop.(none)>
2010-01-12 16:21:27 +0100

    antal billeder, samt afprøvning ændring

--- doc/rapport/afsnit/resultater/parser.py
+++ doc/rapport/afsnit/resultater/parser.py
@@ -1,13 +1,13 @@
 def main():
-	yerfile = open("nation_snitsU.txt")
+	yerfile = open("nation_snits.txt")
 	lines = yerfile.readlines()[0]
 	#print lines
 	lines = lines.split(")")
 	lines.pop(16)
 	size = len(lines)
-	newfile0 = open('csv/nationcutU.csv' , 'w' )
+	newfile0 = open('csv/nationNrImage.csv' , 'w' )
 	newfile0.write("\"ratios\",\"interessante regioner\"\n")
-	nulist = (lines[12],lines[10],lines[3],lines[8],lines[0],lines[6],lines[13],lines[5],lines[15],lines[2],lines[14],lines[7],lines[9],lines[11],lines[1],lines[4])
+	nulist = lines#(lines[12],lines[10],lines[3],lines[8],lines[0],lines[6],lines[13],lines[5],lines[15],lines[2],lines[14],lines[7],lines[9],lines[11],lines[1],lines[4])
 	for line in nulist:
 		number = float(line.rstrip("(").split("}")[5].lstrip(", "))
 		year = line.rstrip("(").lstrip(", ").split(" ")[0].rstrip("\\r\\n':").lstrip("'")
@@ -19,7 +19,7 @@ def main():
 		vertikal1 = line.rstrip("(").split("}")[2].lstrip(", {").lstrip("{").split(",")
 		vertikal2 = line.rstrip("(").split("}")[3].lstrip(", {").lstrip("{").split(",")
 		regionsum = float(hoisontal1[-1].split(": ")[1]) + float(hoisontal2[-1].split(": ")[1])+float(vertikal1[-1].split(": ")[1])+float(vertikal2[-1].split(": ")[1])
-		newfile0.write(str(year) + ', ' + str(regionsum/number) + "\n")
+		newfile0.write(str(year) + ', ' + str(number) + "\n")
 		dimontion = line.rstrip("(").split("}")[4].lstrip("], {")
 
 	print number
