a874ad8b82144be7860673333ca5bc813c5cfd2d
Steenstrup <steenstrup@steenstrup-laptop.(none)>
2010-01-12 15:01:25 +0100

    fra 4 til en i year og nation samt færdi med beskrivelse af dem

--- doc/rapport/afsnit/resultater/parser.py
+++ doc/rapport/afsnit/resultater/parser.py
@@ -1,19 +1,13 @@
 def main():
-	yerfile = open("nation_snits.txt")
+	yerfile = open("nation_snitsU.txt")
 	lines = yerfile.readlines()[0]
 	#print lines
 	lines = lines.split(")")
 	lines.pop(16)
 	size = len(lines)
-	newfile0 = open('csv/nationcut0.csv' , 'w' )
-	newfile1 = open('csv/nationcut1.csv' , 'w' )
-	newfile2 = open('csv/nationcut2.csv' , 'w' )
-	newfile3 = open('csv/nationcut3.csv' , 'w' )
+	newfile0 = open('csv/nationcutU.csv' , 'w' )
 	newfile0.write("\"ratios\",\"interessante regioner\"\n")
-	newfile1.write("\"ratios\",\"interessante regioner\"\n")
-	newfile2.write("\"ratios\",\"interessante regioner\"\n")
-	newfile3.write("\"ratios\",\"interessante regioner\"\n")
-	nulist = lines #(lines[12],lines[10],lines[3],lines[8],lines[0],lines[6],lines[13],lines[5],lines[15],lines[2],lines[14],lines[7],lines[9],lines[11],lines[1],lines[4])
+	nulist = (lines[12],lines[10],lines[3],lines[8],lines[0],lines[6],lines[13],lines[5],lines[15],lines[2],lines[14],lines[7],lines[9],lines[11],lines[1],lines[4])
 	for line in nulist:
 		number = float(line.rstrip("(").split("}")[5].lstrip(", "))
 		year = line.rstrip("(").lstrip(", ").split(" ")[0].rstrip("\\r\\n':").lstrip("'")
@@ -21,17 +15,11 @@ def main():
 			continue
 		
 		hoisontal1 = line.rstrip("(").split("}")[0].lstrip("'" +year+ "': ([").lstrip("{").split(",")
-		newfile0.write(str(year) + ', ' + str(float(hoisontal1[-1].split(": ")[1])/number) + "\n")
-
 		hoisontal2 = line.rstrip("(").split("}")[1].lstrip(", {").lstrip("{").split(",")
-		newfile1.write(str(year) + ', ' + str(float(hoisontal2[-1].split(": ")[1])/number) + "\n")
-
 		vertikal1 = line.rstrip("(").split("}")[2].lstrip(", {").lstrip("{").split(",")
-		newfile2.write(str(year) + ', ' + str(float(vertikal1[-1].split(": ")[1])/number) + "\n")
-	
 		vertikal2 = line.rstrip("(").split("}")[3].lstrip(", {").lstrip("{").split(",")
-		newfile3.write(str(year) + ', ' + str(float(vertikal2[-1].split(": ")[1])/number) + "\n")
-
+		regionsum = float(hoisontal1[-1].split(": ")[1]) + float(hoisontal2[-1].split(": ")[1])+float(vertikal1[-1].split(": ")[1])+float(vertikal2[-1].split(": ")[1])
+		newfile0.write(str(year) + ', ' + str(regionsum/number) + "\n")
 		dimontion = line.rstrip("(").split("}")[4].lstrip("], {")
 
 	print number
